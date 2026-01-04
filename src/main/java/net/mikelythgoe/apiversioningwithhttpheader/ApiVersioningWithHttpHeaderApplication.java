ccc5a1d442e7280d49c3bffcc90e4e300b31b4d6
mlythgoe <github@mikelythgoe.net>
2026-01-04 12:45:19 +0000

    More info in readme.md

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/ApiVersioningWithHttpHeaderApplication.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/ApiVersioningWithHttpHeaderApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiVersioningWithHttpHeaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiVersioningWithHttpHeaderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiVersioningWithHttpHeaderApplication.class, args);
+    }
 
 }
--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
@@ -1,4 +1,19 @@
 package net.mikelythgoe.apiversioningwithhttpheader;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HttpHeaderVersionController {
+
+    @GetMapping(value = "/hello", version = "1.0")
+    public String helloV1() {
+        return "Hello Version One";
+    }
+
+    @GetMapping(value = "/hello", version = "2.0")
+    public String helloV2() {
+        return "Hello Version Two";
+    }
+
 }
