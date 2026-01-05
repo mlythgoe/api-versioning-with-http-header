4341a97675378fa96740b63be74cbb0531b5ca7c
mikelythgoe <github@mikelythgoe.net>
2026-01-05 12:25:07 +0000

    added comments about the major.minor.patch formatting

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
@@ -6,18 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpHeaderVersionController {
 
-    @GetMapping(value = "/hello", version = "1")
+    @GetMapping(value = "/hello", version = "1") // version of "1", "1.0" or "1.0.0" will work, "1.1", "1.0.1", etc., will not work
     public String helloV1() {
         return "Hello Version One";
     }
 
+    @GetMapping(value = "/hello", version = "9")
+    public String helloV9() {
+        return "Hello Version 9";
+    }
 
     @GetMapping(value = "/hello", version = "9.9")
     public String helloV99() {
         return "Hello Version 9.9";
     }
 
-// The methods below fails - the app won't even start - the version must be major.minor<.patch> numbers
+// The method below fails - the app won't even start!!!
+// The version MUST be major<.minor<.patch>> format where all parts, if used, are integers
 //    @GetMapping(value = "/hello", version = "x.x")
 //    public String helloVXx() {
 //        return "Hello Version x.x";
