19d454feac2bc83616d1b274842be5d44bd0f1d2
mikelythgoe <github@mikelythgoe.net>
2026-01-06 09:29:15 +0000

    make code and comments consistent with other projects for rest api versioning.

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
@@ -5,20 +5,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HttpHeaderVersionController {
+    // For versioning, version must be major.minor.patch format
 
-    // http://localhost:8080/hello http header X-API-Version=1.0 (you can use 1.0, 1, or 1.0.0, but you can't use 1.1, 1.0.1 etc.)
+    // http://localhost:8080/hello http header X-API-Version=1.0 (you can use 1.0, 1, or 1.0.0, but you can't use 1.1, 1.0.1, etc.)
     @GetMapping(value = "/hello", version = "1")
     public String helloV1() {
         return "Hello Version 1";
     }
 
-    // http://localhost:8080/hello http header X-API-Version=2.0 (you can use 2.0, 2, or 2.0.0, but you can't use 2.1, 2.0.1 etc.)
+    // http://localhost:8080/hello http header X-API-Version=2.0 (you can use 2.0, 2, or 2.0.0, but you can't use 2.1, 2.0.1, etc.)
     @GetMapping(value = "/hello", version = "2.0")
     public String helloV2point0() {
         return "Hello Version 2";
     }
 
-    // http://localhost:8080/hello, http header X-API-Version=3.5 (you can use 3.5, or 3.5.0, but you can't use 3.6., 3.5.1, etc.)
+    // http://localhost:8080/hello, http header X-API-Version=3.5 (you can use 3.5, or 3.5.0, but you can't use 3, 3.6., 3.5.1, etc.)
     @GetMapping(value = "/hello", version = "3.5")
     public String helloV3point5() {
         return "Hello Version 3.5";
