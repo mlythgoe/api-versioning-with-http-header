134f7adfc085de43bba017b534314e19f955fb8c
mlythgoe <github@mikelythgoe.net>
2026-01-09 21:53:39 +0000

    Tweaked syntax and use text blocks

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
@@ -10,38 +10,62 @@ public class HttpHeaderVersionController {
     // http://localhost:8080/hello http header X-API-Version=1.0 (you can use 1.0, 1, or 1.0.0, but you can't use 1.1, 1.0.1, etc.)
     @GetMapping(value = "/hello", version = "1")
     public String helloV1() {
-        return "{\"message\":\"Hello Version 1\"}";
+        return """
+                {
+                "message":"Hello Version 1"
+                }
+                """;
     }
 
     // http://localhost:8080/hello http header X-API-Version=2.0 (you can use 2.0, 2, or 2.0.0, but you can't use 2.1, 2.0.1, etc.)
     // Example: curl --location 'http://localhost:8080/hello' --header 'X-Api-Version: 2.0'
     @GetMapping(value = "/hello", version = "2.0")
     public String helloV2point0() {
-        return "{\"message\":\"Hello Version 2\"}";
+        return """
+                {
+                "message":"Hello Version 2"
+                }
+                """;
     }
 
     // http://localhost:8080/hello, http header X-API-Version=3.5 (you can use 3.5, or 3.5.0, but you can't use 3, 3.6., 3.5.1, etc.)
     @GetMapping(value = "/hello", version = "3.5")
     public String helloV3point5() {
-        return "{\"message\":\"Hello Version 3.5\"}";
+        return """
+                {
+                "message":"Hello Version 3.5"
+                }
+                """;
     }
 
     // http://localhost:8080/hello http header X-API-Version=9 (you can use 9, 9.0, or 9.0.0, but you can't use 9.1, 9.0.1, etc.)
     @GetMapping(value = "/hello", version = "9")
     public String helloV9() {
-        return "{\"message\":\"Hello Version 9\"}";
+        return """
+                {
+                "message":"Hello Version 9"
+                }
+                """;
     }
 
     // http://localhost:8080/hello http header X-API-Version=9.9 (you can use 9.9, or 9.9.0, but you can't use 9.1, 9.9.1, etc.)
     @GetMapping(value = "/hello", version = "9.9")
     public String helloV9point9() {
-        return "{\"message\":\"Hello Version 9.9\"}";
+        return """
+                {
+                "message":"Hello Version 9.9"
+                }
+                """;
     }
 
     // http://localhost:8080/hello http header X-API-Version=9.9.9 (you can use 9.9.9, or 9.9.9.0, but you can't use 9.9.1, 9.9.9.1, etc.)
     @GetMapping(value = "/hello", version = "9.9.9")
     public String helloV9point9point9() {
-        return "{\"message\":\"Hello Version 9.9.9\"}";
+        return """
+                {
+                "message":"Hello Version 9.9.9"
+                }
+                """;
     }
 
     // The method below fails - the app won't even start!!!
