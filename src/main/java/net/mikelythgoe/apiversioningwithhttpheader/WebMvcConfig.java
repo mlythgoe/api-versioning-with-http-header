9eceb57c6cafe3b7fd3e6fce6e2512803668e5c0
mlythgoe <github@mikelythgoe.net>
2026-01-10 10:36:03 +0000

    WebConfig defines that version MUST be supplied - there is no default allowed

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/WebMvcConfig.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/WebMvcConfig.java
@@ -9,13 +9,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private static final String API_VERSION_HEADER = "X-API-Version";
     private static final String[] SUPPORTED_VERSIONS = {"1.0", "2.0", "3.5", "9"};
-    private static final String DEFAULT_VERSION = "2.0";
 
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
 
+        // In this project, we set 'Version Required' to true,
+        // meaning the version MUST be passed, and there is no default, so the default version has been removed
+        // from the configuration.
         configurer
                 .addSupportedVersions(SUPPORTED_VERSIONS)
-                .setDefaultVersion(DEFAULT_VERSION)
+                .setVersionRequired(true)
                 .useRequestHeader(API_VERSION_HEADER);
     }
 }
