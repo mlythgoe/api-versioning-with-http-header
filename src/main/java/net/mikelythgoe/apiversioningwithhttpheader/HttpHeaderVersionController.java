ef120c50c9bac30f1f5d50c279bf4becff89b31b
mikelythgoe <github@mikelythgoe.net>
2026-01-16 15:01:58 +0000

    Changed controller to set mapping at controller level, rather than at method level (which can still be used to refine the mapping url if required. It does show that setting the version on a method can be done seprately to the url mapping

--- src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
+++ src/main/java/net/mikelythgoe/apiversioningwithhttpheader/HttpHeaderVersionController.java
@@ -2,15 +2,17 @@ package net.mikelythgoe.apiversioningwithhttpheader;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HttpHeaderVersionController {
     // For versioning, the version must be major.minor.patch format
 
     // http://localhost:8080/hello http header X-API-Version=1.0 (you can use 1.0, 1, or 1.0.0, but
     // you can't use 1.1, 1.0.1, etc.)
-    @GetMapping(value = "/hello", version = "1")
+    @GetMapping(version = "1")
     public ResponseEntity<Message> helloV1() {
         return ResponseEntity.ok(new Message("Hello Version 1"));
     }
@@ -18,28 +20,28 @@ public class HttpHeaderVersionController {
     // http://localhost:8080/hello http header X-API-Version=2.0 (you can use 2.0, 2, or 2.0.0, but
     // you can't use 2.1, 2.0.1, etc.)
     // Example: curl --location 'http://localhost:8080/hello' --header 'X-Api-Version: 2.0'
-    @GetMapping(value = "/hello", version = "2.0")
+    @GetMapping(version = "2.0")
     public ResponseEntity<Message> helloV2point0() {
         return ResponseEntity.ok(new Message("Hello Version 2"));
     }
 
     // http://localhost:8080/hello, http header X-API-Version=3.5 (you can use 3.5, or 3.5.0, but
     // you can't use 3, 3.6., 3.5.1, etc.)
-    @GetMapping(value = "/hello", version = "3.5")
+    @GetMapping(version = "3.5")
     public ResponseEntity<Message> helloV3point5() {
         return ResponseEntity.ok(new Message("Hello Version 3.5"));
     }
 
     // http://localhost:8080/hello http header X-API-Version=9 (you can use 9, 9.0, or 9.0.0, but
     // you can't use 9.1, 9.0.1, etc.)
-    @GetMapping(value = "/hello", version = "9")
+    @GetMapping(version = "9")
     public ResponseEntity<Message> helloV9() {
         return ResponseEntity.ok(new Message("Hello Version 9"));
     }
 
     // http://localhost:8080/hello http header X-API-Version=9.9 (you can use 9.9, or 9.9.0, but you
     // can't use 9.1, 9.9.1, etc.)
-    @GetMapping(value = "/hello", version = "9.9+")
+    @GetMapping(version = "9.9+")
     public ResponseEntity<Message> helloV9point9() {
         return ResponseEntity.ok(new Message("Hello Version 9.9"));
     }
